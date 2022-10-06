34ca5c85e01f696e39ae01aab0c972ae666dd8ae
QuocHuy <ngdquochuy241@gmail.com>
2022-10-06 17:20:08 +0700

    thực hành 2

---
+++ Bai_1/customer-management/src/main/java/cg/wbd/grandemonstration/configuration/ApplicationConfig.java
@@ -0,0 +1,24 @@
+package cg.wbd.grandemonstration.configuration;
+
+
+import cg.wbd.grandemonstration.service.CustomerService;
+import cg.wbd.grandemonstration.service.impl.SimpleCustomerServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+public class ApplicationConfig extends WebMvcConfigurerAdapter {
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/templates/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Bean
+    public CustomerService customerService() {
+        return new SimpleCustomerServiceImpl();
+    }
+}
